10c7b8e06690905432827c4ebc30a5e77f20d4dc
Jonathan Carroll <jono@jcarroll.com.au>
2016-06-09 00:42:28 +0930

    dropped formatting

---
+++ test_output.R
@@ -0,0 +1,49 @@
+library(githuburlcheckr)
+output <- test.urls("githuburlcheckr-test", "draft.md")
+#> Scanning for githuburlcheckr-test
+#> Found repository: githuburlcheckr-test, scanning draft.md
+#> Found 42 URLs.
+output
+href	code
+http://r-norberg.blogspot.com/2016/06/understanding-datatabl…	200
+http://www.win-vector.com/blog/2016/06/using-geom_step/…	200
+https://ellisp.github.io/blog/2016/06/05/bootstrap-cv-strate…	200
+http://casualinference.org/2016/06/04/bigmemory_rcppeigen/…	200
+http://datascienceplus.com/handling-missing-data-with-mice-p…	200
+http://ipub.com/r-profiling/…	200
+http://hoehleatsu.github.io/2016/05/22/proofCalculation.html…	200
+https://hoehleatsu.github.io/2016/05/06/when2stop.html…	200
+http://testingfuntime.blogspot.co.uk/2016/06/a-study-of-soft…	200
+https://412sportsanalytics.wordpress.com/2016/06/05/understa…	200
+http://www-scf.usc.edu/~ursaner/DreamAnalysis.html…	200
+https://bichopsuey.wordpress.com/2016/06/05/oil-price-trend-…	200
+http://r4stats.com/2016/06/06/rexer-data-science-survey-sati…	200
+https://aschinchon.wordpress.com/2016/06/06/women-in-orchest…	200
+http://blog.datascienceheroes.com/time-series-analysis-profi…	200
+http://www.r-bloggers.com/what-are-the-best-machine-learning…	200
+http://joelcadwell.blogspot.my/2016/06/building-data-matrix-…	200
+https://gigadom.wordpress.com/2016/06/05/exploring-quantum-g…	200
+https://www.r-consortium.org/news/announcement/2016/06/ibm-i…	200
+https://support.bioconductor.org/p/83448/…	200
+http://onlinelibrary.wiley.com/doi/10.1111/2041-210X.12600/f…	405
+https://cran.r-project.org/web/packages/camtrapR/…	200
+https://ropensci.github.io/biweekly/update-2016-06-06/…	200
+https://www.youtube.com/watch?list=PL_LGEn4G1x8r99HkY_RfJNlD…	200
+https://www.youtube.com/watch?v=Vly8xGnNiWs…	200
+http://datascience.la/xgboost-workshop-and-meetup-talk-with-…	200
+http://smarterpoland.pl/index.php/2016/06/frequency-analysis…	200
+https://github.com/MangoTheCat/shinytoastr…	200
+https://cdn.rawgit.com/MangoTheCat/shinytoastr/master/inst/s…	200
+https://github.com/MangoTheCat/shinytoastr…	200
+http://www.arilamstein.com/blog/2016/06/06/idbr-access-us-ce…	200
+https://cran.r-project.org/web/packages/idbr/…	200
+http://developer.r-project.org/blosxom.cgi/R-devel/NEWS…	200
+http://user2016.org/…	200
+https://twitter.com/hashtag/user2016…	200
+http://schedule.user2016.org](http://schedule.user2016.org…	0
+https://twitter.com/samfirke/status/739845326123700224…	200
+https://pbs.twimg.com/media/CkR1ZrNWYAADgSe.jpg:large…	200
+https://twitter.com/samfirke/status/739845326123700224…	200
+https://www.r-project.org/…	200
+https://github.com/rweekly/rweekly.org…	200
+https://twitter.com/rweekly_org…	200
